a6b468d4addd47d9d81b44667e41227d2c5f6371
jolaya182 <jolaya182@gmail.com>
2020-11-08 19:50:28 -0500

    upedated index description

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css'
